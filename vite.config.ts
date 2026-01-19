ece6a9e1e2494848e56772622ee1b2dc50d3973e
Matt Voget <matt.voget@gmail.com>
2026-01-18 21:27:36 -0700

    fix: format files to match spec exactly

--- vite.config.ts
+++ vite.config.ts
@@ -23,16 +23,8 @@ export default defineConfig({
         background_color: '#0a0e14',
         display: 'standalone',
         icons: [
-          {
-            src: '/icon-192.png',
-            sizes: '192x192',
-            type: 'image/png'
-          },
-          {
-            src: '/icon-512.png',
-            sizes: '512x512',
-            type: 'image/png'
-          }
+          { src: '/icon-192.png', sizes: '192x192', type: 'image/png' },
+          { src: '/icon-512.png', sizes: '512x512', type: 'image/png' }
         ]
       }
     })
